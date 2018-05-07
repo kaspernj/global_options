333157cef04e86c928115fc3b94402d968ef85de
kaspernj <kaspernj@gmail.com>
2018-05-07 11:39:55 +0000

    Simplified and fixed raise issues

--- app/models/global_options/global_option.rb
+++ app/models/global_options/global_option.rb
@@ -4,12 +4,6 @@ class GlobalOptions::GlobalOption < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: true
 
-  def self.by_name(name)
-    option = where(name: name).first
-    raise NotFound, "Option not found by that name: '#{name}'." unless option
-    return option
-  end
-
   def self.set(name, value)
     option = find_or_initialize_by(name: name)
     option.value = value
@@ -17,6 +11,10 @@ class GlobalOptions::GlobalOption < ActiveRecord::Base
   end
 
   def self.get(name)
-    by_name(name).value
+    find_by(name: name)&.value
+  end
+
+  def self.get!(name)
+    find_by!(name: name).value
   end
 end
